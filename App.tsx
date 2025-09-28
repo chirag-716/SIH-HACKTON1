ee2b58933132f93beea445afd143cc0c43e46a5f
chirag-716 <chavdac249@gmail.com>
2025-09-28 19:34:10 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello, CodeSandbox!</h1>
+      <p>Your preview is working 🎉</p>
+    </div>
+  );
+}
+
 
